706dc92a8791d620ade6fe0c67c4f81e9ce5eef3
Gunnar Unger <gunnar.unger@gmail.com>
2021-05-17 09:21:42 +1000

    update

--- module-list.js
+++ module-list.js
@@ -1,10 +1,11 @@
 (function(){
     var modules={
-        "panel-main-nrs":    			 {url:"$H/m/elig-questions-data.html",Table:"nrs-recruitment-record",form_module:"elig-questions-form"},
-        "elig-questions-form":    			 {url:"$H/m/elig-questions-form.html",Table:"nrs-recruitment-record"},
-        "panel-main-nrs-eligible":      {url:"$H/m/elig-questions-eligible-data.html",Table:"nrs-recruitment-record",form_module:"panel-main-nrs-eligible-form"},
-        "panel-main-nrs-eligible-form":		    {url:"$H/m/elig-questions-eligible-form.html",Table:"nrs-recruitment-record"},
-        "panel-main-nrs-non-eligible":  {url:"$H/m/elig-questions-non-eligible-data.html",Table:"nrs-recruitment-record"},
+        "panel-main-sdem":    			 {url:"$H/m/elig-questions-data.html",Table:"sdem-recruitment-record",form_module:"elig-questions-form"},
+        "elig-questions-form":    			 {url:"$H/m/elig-questions-form.html",Table:"sdem-recruitment-record"},
+        "panel-main-sdem-eligible":      {url:"$H/m/elig-questions-eligible-data.html",Table:"sdem-recruitment-record",form_module:"panel-main-sdem-eligible-form"},
+        "panel-main-sdem-eligible-form":		    {url:"$H/m/elig-questions-eligible-form.html",Table:"sdem-recruitment-record"},
+        "panel-main-sdem-non-eligible":  {url:"$H/m/elig-questions-non-eligible-data.html",Table:"sdem-recruitment-record"},
+        "sdem-comments":            {url:"$H/m/comments-form.html",Table:"sdem-recruitment-record",task_name:"Comments"},
     }
     //for(m in modules){$vm.module_list[m]=modules[m];}
 
@@ -13,10 +14,10 @@
         $vm.module_list[p]=modules[p];
         $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
     }
-    if(window.location.toString().indexOf('tb=demo')!=-1){
+    //if(window.location.toString().indexOf('tb=demo')!=-1){
         for(p in $vm.module_list){
             $vm.module_list[p].Table="demo-"+$vm.module_list[p].Table;
         }
-    }
+    //}
 
 })();
